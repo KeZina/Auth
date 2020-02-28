94d4ffcc3d06b69369c97ba695cfe692d4836a87
Artyom K <kezina2036@gmail.com>
2020-02-28 20:57:05 +0300

    add <Title />

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from './Title';
+
+const App = () => {
+  return (
+    <div id = 'general-container'>
+      <Title />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <header id = 'title'>
+            <div>
+                <div />
+                <span>yourtext</span>
+            </div>
+            <h1>Registration Form</h1>
+        </header>
+    )
+}
+
+export default Title;
